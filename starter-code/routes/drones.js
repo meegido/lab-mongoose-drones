39dd4f03112f924e6456580cab83f18ab2678db3
Mercedes Egido <meegido@gmail.com>
2017-09-08 14:15:50 +0200

    iteration 3 creat new drone

--- starter-code/routes/drones.js
+++ starter-code/routes/drones.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const Drone = require ('../models/drones');
 const router = express.Router();
+const Drone = require ('../models/drones');
+
 
 
 router.get('/', (req, res, next) => {
@@ -15,8 +16,25 @@ router.get('/', (req, res, next) => {
 
 
 router.get('/drones/new', (req, res, next) => {
-  // Iteration #3
+  res.render('drones/new', {
+    title: "Add new drone"
+  })
 });
+
+router.post('/drones/new', (req, res, next) => {
+  let oneMoreDrone = { // objeto normal
+    droneName: req.body.droneName,
+    propellers: req.body.propellers,
+    maxSpeed: req.body.maxSpeed
+  }
+
+  const newDrone = new Drone(oneMoreDrone); // creo una instancia nueva con Schema de Mongoose del modelo Drone
+  newDrone.save((err) => { //estoy usando un método de mongoose
+    if (err) { return next(err); }
+    return res.redirect('/'); //redirijo al root porque es donde he puesto el listado
+  });
+
+})
 
 router.post('/drones', (req, res, next) => {
   // Iteration #3
